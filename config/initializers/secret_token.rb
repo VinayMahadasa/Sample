69f38c9a5ef34e6012537baa8864badf7f77b6fb
VinayMahadasa <VinayMahadasa@github.com>
2013-09-17 23:11:07 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+School::Application.config.secret_key_base = 'f118a2b98838cf230aa4f28721bd4a372ef9214b4a9178acdd68b5cd91e10be60be0c113c036b6d095a7c98053c5802e7dcfceab679afd6f071f11068de33b1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+School::Application.config.session_store :cookie_store, key: '_School_session'
